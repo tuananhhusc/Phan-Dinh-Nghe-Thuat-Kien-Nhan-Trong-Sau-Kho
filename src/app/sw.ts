e0cb157b3a4e72062084203f6df98e7621e07a90
Your Name <you@example.com>
2026-07-02 22:24:10 +0700

    fix: update serwist imports for build and add serwist pkg

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,6 +1,6 @@
 import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry, SerwistGlobalConfig } from "@serwist/precaching";
-import { Serwist } from "@serwist/sw";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import { Serwist } from "serwist";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
